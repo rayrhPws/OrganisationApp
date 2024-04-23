812cf83b1b418b11d0ead5d768369406a0803e89
rayrhPws <arif.naveed.js@gmail.com>
2024-04-23 22:54:56 +0500

    map with dynamic marker added

---
+++ OrganisationModule/Models/MapModel.swift
@@ -0,0 +1,8 @@
+//
+//  MapModel.swift
+//  OrganisationModule
+//
+//  Created by Arif ww on 23/04/2024.
+//
+
+import Foundation
---
+++ OrganisationModule/OrganisationModule/Controllers/Map/MapVc.swift
@@ -0,0 +1,29 @@
+//
+//  MapVc.swift
+//  OrganisationModule
+//
+//  Created by Arif ww on 23/04/2024.
+//
+
+import UIKit
+
+class MapVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
